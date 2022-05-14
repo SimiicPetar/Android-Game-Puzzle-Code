bba43e92b04be89f24b389c364c759769086d3c9
Simic <38137063+SimiicPetar@users.noreply.github.com>
2022-05-14 21:35:03 +0200

    Add files via upload

---
+++ ClickPlugin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tabtale.TTPlugins;
+
+public class ClickPlugin : MonoBehaviour
+{
+
+
+    private void Awake()
+    {
+        // Initialize CLIK Plugin
+        TTPCore.Setup();
+        // Your code here
+    }
+    public void Start()
+
+    {
+        TTPCore.Setup();
+    }
+}
---
+++ Destroy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    public GameObject asd;
+    public float sec;
+
+    void Awake()
+    {
+
+        Destroy(asd, sec);
+    }
+}
